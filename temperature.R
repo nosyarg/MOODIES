5c83fe2735cdf114b2c692badf2e086415d234ea
nosyarg <york17a@ncssm.edu>
2017-02-26 10:15:55 -0500

    created copy of temperature files cleaned for R and started temperature processing script

---
+++ temperature.R
@@ -0,0 +1,12 @@
+#Temperature
+
+library(ggplot2)
+
+setwd("~/Desktop/MOODIES/data/alltemps")
+
+Acadia.Temp <- read.csv("ACADIA.csv")
+Hatteras.Temp <- read.csv("HATTERAS.csv")
+Kenai.Temp <- read.csv("KENAI.csv")
+Olympic.Temp <- read.csv("OLYMPIC.csv")
+Padre.Temp <- read.csv("PADRE.csv")
+
