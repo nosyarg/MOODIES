d2bd63e3370b4a5fd72e8eb5379d7f6e85fefe94
nikhil <nikhil@nikhil-laptop>
2017-02-26 12:53:42 -0500

    New Stuff

--- MeanSeaLevel.R
+++ MeanSeaLevel.R
@@ -36,39 +36,91 @@ qplot(
 nonaacadia = mean.sea.level$Acadia[!is.na(mean.sea.level$Acadia)]
 totalsealevelAcadia = cumsum(nonaacadia)
 
-plot(
+qplot(
   x=mean.sea.level$Year[!is.na(mean.sea.level$Acadia)] + mean.sea.level$Month[!is.na(mean.sea.level$Acadia)] / 12,
-  y= totalsealevelAcadia
-)
+  y= totalsealevelAcadia,
+  xlab="Time",
+  ylab="Cumulative MSL",
+  main="Acadia Cumulative MSL Function"
+) + geom_line()
 
+acadia <- data.frame(
+  totalsealevelAcadia, 
+  mean.sea.level$Year[!is.na(mean.sea.level$Acadia)] + mean.sea.level$Month[!is.na(mean.sea.level$Acadia)] / 12 - 1997,
+  (mean.sea.level$Year[!is.na(mean.sea.level$Acadia)] + mean.sea.level$Month[!is.na(mean.sea.level$Acadia)] / 12 - 1997)^2,
+  (mean.sea.level$Year[!is.na(mean.sea.level$Acadia)] + mean.sea.level$Month[!is.na(mean.sea.level$Acadia)] / 12 - 1997)^3,
+  (mean.sea.level$Year[!is.na(mean.sea.level$Acadia)] + mean.sea.level$Month[!is.na(mean.sea.level$Acadia)] / 12 - 1997)^4,
+  log(mean.sea.level$Year[!is.na(mean.sea.level$Acadia)] + mean.sea.level$Month[!is.na(mean.sea.level$Acadia)] / 12 - 1997))
+names(acadia) <- c("Acadia", "Time", "Time2", "Time3", "Time4", "logTime")
+acadia.fit <- glm(Acadia~logTime, data=acadia)
+summary(acadia.fit)
+                    
 nonahatteras = mean.sea.level$Cape.Hatteras[!is.na(mean.sea.level$Cape.Hatteras)]
 totalsealevelCape.Hatteras = cumsum(nonahatteras)
 
-plot(
+qplot(
   x=mean.sea.level$Year[!is.na(mean.sea.level$Cape.Hatteras)] + mean.sea.level$Month[!is.na(mean.sea.level$Cape.Hatteras)] / 12,
-  y= totalsealevelCape.Hatteras
+  y= totalsealevelCape.Hatteras,
+  xlab="Time",
+  ylab="Cumulative MSL",
+  main="Cape Hatteras Cumulative MSL Function"
+) + geom_line()
+
+hatteras <- data.frame(
+  totalsealevelCape.Hatteras, 
+  mean.sea.level$Year[!is.na(mean.sea.level$Cape.Hatteras)] + mean.sea.level$Month[!is.na(mean.sea.level$Cape.Hatteras)] / 12 - 1997,
+  (mean.sea.level$Year[!is.na(mean.sea.level$Cape.Hatteras)] + mean.sea.level$Month[!is.na(mean.sea.level$Cape.Hatteras)] / 12 - 1997)^2,
+  (mean.sea.level$Year[!is.na(mean.sea.level$Cape.Hatteras)] + mean.sea.level$Month[!is.na(mean.sea.level$Cape.Hatteras)] / 12 - 1997)^3
 )
+names(hatteras) <- c("Hatteras", "Time", "Time2", "Time3")
+hatteras.fit <- glm(Hatteras~Time+Time2+Time3, data=hatteras)
+summary(hatteras.fit)
 
 nonakenai = mean.sea.level$Kenai.Fjords[!is.na(mean.sea.level$Kenai.Fjords)]
 totalsealevelKenai.Fjords = cumsum(nonakenai)
 
-plot(
+qplot(
   x=mean.sea.level$Year[!is.na(mean.sea.level$Kenai.Fjords)] + mean.sea.level$Month[!is.na(mean.sea.level$Kenai.Fjords)] / 12,
-  y= totalsealevelKenai.Fjords
-)
+  y= totalsealevelKenai.Fjords,
+  xlab="Time",
+  ylab="Cumulative MSL",
+  main="Kenai Fjords Cumulative MSL Function"
+) + geom_line()
+
+kenai <- data.frame(
+  totalsealevelKenai.Fjords, 
+  mean.sea.level$Year[!is.na(mean.sea.level$Kenai.Fjords)] + mean.sea.level$Month[!is.na(mean.sea.level$Kenai.Fjords)] / 12 - 1997, 
+  (mean.sea.level$Year[!is.na(mean.sea.level$Kenai.Fjords)] + mean.sea.level$Month[!is.na(mean.sea.level$Kenai.Fjords)] / 12 - 1997)^2,
+  (mean.sea.level$Year[!is.na(mean.sea.level$Kenai.Fjords)] + mean.sea.level$Month[!is.na(mean.sea.level$Kenai.Fjords)] / 12 - 1997)^3)
+names(kenai) <- c("Kenai", "Time", "Time2", "Time3")
+kenai.fit <- lm(Kenai~Time+Time2+Time3, data=kenai)
 
 nonaolympic = mean.sea.level$Olympic[!is.na(mean.sea.level$Olympic)]
 totalsealevelOlympic= cumsum(nonaolympic)
 
-plot(
+qplot(
   x=mean.sea.level$Year[!is.na(mean.sea.level$Olympic)] + mean.sea.level$Month[!is.na(mean.sea.level$Olympic)] / 12,
-  y= totalsealevelOlympic
-)
+  y= totalsealevelOlympic,
+  xlab="Time",
+  ylab="Cumulative MSL",
+  main="Olympic Cumulative MSL Function"
+) + geom_line()
+
+olympic <- data.frame(totalsealevelOlympic, sin(mean.sea.level$Year[!is.na(mean.sea.level$Olympic)] + mean.sea.level$Month[!is.na(mean.sea.level$Olympic)] / 12 - 1997))
+names(olympic) <- c("Olympic", "sinTime")
+olympic.fit <- glm(Olympic~sinTime, data=olympic)
 
 nonapadre = mean.sea.level$Padre.Island[!is.na(mean.sea.level$Padre.Island)]
 totalsealevelPadre.Island= cumsum(nonapadre)
 
-plot(
+qplot(
   x=mean.sea.level$Year[!is.na(mean.sea.level$Padre.Island)] + mean.sea.level$Month[!is.na(mean.sea.level$Padre.Island)] / 12,
-  y= totalsealevelPadre.Island
-)
+  y= totalsealevelPadre.Island,
+  xlab="Time",
+  ylab="Cumulative MSL",
+  main="Padre Island Cumulative MSL Function"
+) + geom_line()
+
+padre <- data.frame(totalsealevelPadre.Island, mean.sea.level$Year[!is.na(mean.sea.level$Padre.Island)] + mean.sea.level$Month[!is.na(mean.sea.level$Padre.Island)] / 12 - 1997, sin(mean.sea.level$Year[!is.na(mean.sea.level$Padre.Island)] + mean.sea.level$Month[!is.na(mean.sea.level$Padre.Island)] / 12 - 1997))
+names(padre) <- c("Padre", "Time", "sinTime")
+padre.fit <- glm(Padre~Time+sinTime, data=padre)
